2393232f51b590af97fd5a9840147b3944b2e766
Tulagunsmith <tulagunsmith@gmail.com>
2022-06-18 17:37:09 +0300

    import changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from netology_file_work import file_work_1
+import file_work_1
 
 
 if __name__ == '__main__':
