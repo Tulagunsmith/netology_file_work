cd11bd24629969b43da683175ca79f9ddc223c38
Tulagunsmith <tulagunsmith@gmail.com>
2022-06-18 16:18:38 +0300

    main.py added

---
+++ main.py
@@ -0,0 +1,9 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from netology_file_work import file_work_1
+
+
+if __name__ == '__main__':
+    print(file_work_1)
+
