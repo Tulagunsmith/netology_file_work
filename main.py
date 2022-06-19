76a6e5c0e6eb6ec55c13b9087ba00e6c331182ff
Tulagunsmith <tulagunsmith@gmail.com>
2022-06-19 11:56:47 +0300

    changed import in main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import file_work_1
+import file_work_2
 
 
 if __name__ == '__main__':
-    print(file_work_1)
+    print(file_work_2)
 
