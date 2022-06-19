55852abd7a62d68339b0c9ad69d08231c9c7dc64
Tulagunsmith <tulagunsmith@gmail.com>
2022-06-19 18:57:14 +0300

    unnecessary line removed. It's alive!

--- file_work_2.py
+++ file_work_2.py
@@ -1,9 +1,3 @@
-import os
-
-base_path = os.getcwd()
-file_name = 'file_work_1.py'
-full_path = os.path.join(base_path, file_name)
-
 from file_work_1 import cook_book
 
 
@@ -22,48 +16,23 @@ def dict_to_str(dict_):
 
 
 def get_shop_list_by_dishes(dishes, person_count):
-    shop_list = {}
-    count = 0
+    order_list = {}
     person = person_count
     for i in range(len(dishes)):
         if dishes[i] in cook_book:
             for j in range(len(cook_book[dishes[i]])):
-                if len(dishes) != len(set(dishes)):
-                    dictionary = dict_to_dict(cook_book[dishes[i]][j], person)
-                    dict_key = dict_to_str(dictionary)
-                    if dict_key not in shop_list:
-                        shop_list.update(dictionary)
+                dictionary = dict_to_dict(cook_book[dishes[i]][j], person)
+                dict_key = dict_to_str(dictionary)
+                if dict_key not in order_list:
+                    order_list.update(dictionary)
 
-                    else:
-                        dictionary[dict_key]['quantity'] = str(int(dictionary[dict_key]['quantity']) + int(shop_list[dict_key]['quantity']))
+                else:
+                    dictionary[dict_key]['quantity'] = str(
+                        int(dictionary[dict_key]['quantity']) + int(order_list[dict_key]['quantity']))
+                    order_list.update(dictionary)
 
-
-
-                        # print(dictionary[dict_key]['quantity'])
-                        # print()
-                        # print(shop_list[dict_key]['quantity'])
-
-                        # print(dictionary)
-                        # print(dict_key, len(dict_key))
-                        # for key, values in dictionary.items():
-                        #     count = int(values['quantity'])
-                        #     print('dictionary quantity: ', values['quantity'])
-                        # for key, values in shop_list.items():
-                        #     values['quantity'] = int(values['quantity']) + count
-                        #     count = 0
-
-                        # for key, values in dictionary.items():
-                        #     count = int(values['quantity'])
-                        #     print(values['quantity'])
-                        #     print(count)
-                        #     for key, values in shop_list.items():
-                        #         values['quantity'] = int(values['quantity']) + count
-                        #         count = 0
-                        shop_list.update(dictionary)
-
-    return shop_list
+    return order_list
 
 
-shop_list = get_shop_list_by_dishes(['Омлет', 'Омлет'], 1)
+shop_list = get_shop_list_by_dishes(['Омлет', 'Омлет', 'Запеченный картофель', 'Фахитос'], 1)
 print(shop_list)
-
