161f489e5943458500f9c10d78f70858b41bfcf7
Tulagunsmith <tulagunsmith@gmail.com>
2022-06-19 18:48:57 +0300

    crap variant, Frankenstein seems to be alive, condition branching added

--- file_work_2.py
+++ file_work_2.py
@@ -9,22 +9,61 @@ from file_work_1 import cook_book
 
 def dict_to_dict(dish_dict, person_count):
     piece_of_order = {}
-    ingr_info = {'measure': dish_dict['measure'], 'quantity': f'{int(dish_dict["quantity"]) * person_count}'}
+    ingr_info = {'measure': dish_dict['measure'].strip(), 'quantity': f'{int(dish_dict["quantity"]) * person_count}'}
     key_ingr = dish_dict['ingredient_name']
     piece_of_order[key_ingr] = ingr_info
     return piece_of_order
 
 
+def dict_to_str(dict_):
+    keys, *values = dict_.items()
+    str_ = keys[0]
+    return str_
+
+
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
+    count = 0
     person = person_count
     for i in range(len(dishes)):
         if dishes[i] in cook_book:
             for j in range(len(cook_book[dishes[i]])):
-                dictionary = dict_to_dict(cook_book[dishes[i]][j], person)
-                shop_list.update(dictionary)
+                if len(dishes) != len(set(dishes)):
+                    dictionary = dict_to_dict(cook_book[dishes[i]][j], person)
+                    dict_key = dict_to_str(dictionary)
+                    if dict_key not in shop_list:
+                        shop_list.update(dictionary)
+
+                    else:
+                        dictionary[dict_key]['quantity'] = str(int(dictionary[dict_key]['quantity']) + int(shop_list[dict_key]['quantity']))
+
+
+
+                        # print(dictionary[dict_key]['quantity'])
+                        # print()
+                        # print(shop_list[dict_key]['quantity'])
+
+                        # print(dictionary)
+                        # print(dict_key, len(dict_key))
+                        # for key, values in dictionary.items():
+                        #     count = int(values['quantity'])
+                        #     print('dictionary quantity: ', values['quantity'])
+                        # for key, values in shop_list.items():
+                        #     values['quantity'] = int(values['quantity']) + count
+                        #     count = 0
+
+                        # for key, values in dictionary.items():
+                        #     count = int(values['quantity'])
+                        #     print(values['quantity'])
+                        #     print(count)
+                        #     for key, values in shop_list.items():
+                        #         values['quantity'] = int(values['quantity']) + count
+                        #         count = 0
+                        shop_list.update(dictionary)
+
     return shop_list
 
-shop_list = get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
+
+shop_list = get_shop_list_by_dishes(['Омлет', 'Омлет'], 1)
 print(shop_list)
 
