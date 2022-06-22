383b67954d0ba6a2a5cb89b848d89952a104a974
Tulagunsmith <tulagunsmith@gmail.com>
2022-06-22 23:44:51 +0300

    clear name to 3rd program given

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import chimera
+import chimera_file_work_3
 
 
 if __name__ == '__main__':
